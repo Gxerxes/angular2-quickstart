fd53941314dd6159328edddd6975e4e2b1a3fb7b
Gxerxes <xuninglee@163.com>
2016-06-21 09:36:11 +0800

    add login

---
+++ app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'login',
+    templateUrl: 'app/login/login.component.html'
+})
+export class LoginComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
