27b181446a9718601df981d8e04216fdf030f75e
Gxerxes <xuninglee@163.com>
2016-06-22 09:17:43 +0800

    add nprogress

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {PopupComponent} from "./popup/popup.component";
 import {TransclusionComponent} from "./transclusion/transclusion.component";
+
+declare var NProgress: any;
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     directives:[PopupComponent, TransclusionComponent]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+
+    ngOnInit() {
+        NProgress.start();
+
+        setTimeout(NProgress.done, 3000);
+    }
+}
